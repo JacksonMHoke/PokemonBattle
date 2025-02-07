4a0797e9835f9973467d8f2f8a0f8394dd70ad46
JacksonMHoke <jacksonmhoke@gmail.com>
2025-02-06 23:30:48 -0800

    Added enums for typing and stats

---
+++ globals.py
@@ -0,0 +1,28 @@
+from enum import Enum
+class Stat(Enum):
+    MHP=1
+    ATT=2
+    SPA=3
+    DEF=4
+    SPD=5
+    SPE=6
+
+class Type(Enum):
+    NORMAL=1
+    FIGHTING=2
+    FIRE=3
+    WATER=4
+    GRASS=5
+    ELECTRIC=6
+    ICE=7
+    POISON=8
+    GROUND=9
+    FLYING=10
+    PSYCHIC=11
+    BUG=12
+    ROCK=13
+    GHOST=14
+    DARK=15
+    DRAGON=16
+    STEEL=17
+    FAIRY=18
