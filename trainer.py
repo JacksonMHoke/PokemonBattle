8c902a024fdc1b57a6317570bcb96662decffa4d
JacksonMHoke <jacksonmhoke@gmail.com>
2025-02-07 01:49:00 -0800

    Added first pokemon selection function

--- trainer.py
+++ trainer.py
@@ -1,8 +1,8 @@
 from pokemon import Pokemon
 class Trainer:
-    def __init__(self, name, pokemon, bag):
+    def __init__(self, name, party, bag):
         self.name=name
-        self.pokemon=pokemon
+        self.party=party
         self.bag=bag
         self.activePokemon=None
         self.alivePokemon=len(self.pokemon)
@@ -18,3 +18,18 @@ class Trainer:
         except:
             return self.selectMove()
         return validMoves[min(max(0, choice), len(validMoves))]
+    
+    def _getAlivePokemon(self):
+        return [mon for mon in self.party if not mon.fainted]
+    
+    def selectPokemon(self):
+        assert self.alivePokemon>0
+        try:
+            validPokemon=self._getAlivePokemon()
+            print('List of possible pokemon: ')
+            for i, mon in enumerate(validPokemon):
+                print('\t', i+1, mon.name)
+            choice=int(input('Select the pokemon you want to send out by number: '))
+        except:
+            return self.selectPokemon()
+        return validPokemon[min(max(0, choice-1), len(validPokemon)-1)]
