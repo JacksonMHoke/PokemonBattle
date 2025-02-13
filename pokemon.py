1010fa0001979623ed0e45c94c53ebad4c498a8e
JacksonMHoke <jacksonmhoke@gmail.com>
2025-02-13 09:33:41 -0800

    Added documentation for pokemon

--- pokemon.py
+++ pokemon.py
@@ -3,6 +3,22 @@ from globals import Type
 from copy import deepcopy
 from abc import ABC, abstractmethod
 class Pokemon(ABC):
+    """Represents Pokemon
+
+    Attributes:
+        name (str): Nickname of pokemon
+        baseStats (dict): Dictionary of base stats
+        stats (dict): Dictionary of current stats
+        moves (list): List of moves
+        ability (Ability): Pokemon's ability TODO: implement Ability class
+        item (Item): Pokemon's held item TODO: implement Item class
+        fainted (bool): Pokemon's fainted status TODO: implement ENUM for Fainted, Benched, Active
+        level (int): Pokemon's level
+        exp (int): Pokemon's EXP
+        typing (list): List of types
+
+    Note: Pokemon is an abstract class and should not be instantiated. Typing is defined in subclasses.
+    """
     def __init__(self, name, level, baseStats, moves, item=None, ability=None):
         self.name=name
         self.baseStats=deepcopy(baseStats)
@@ -15,10 +31,12 @@ class Pokemon(ABC):
         self.exp=0
 
     def faint(self):
+        """Print fainted message and update state."""
         print(f'{self.name} has fainted!', flush=True)
         self.fainted=True
 
     def takeDamage(self, dmg):
+        """Take damage"""
         self.stats[Stat.HP]-=dmg
         print(f'{self.name} took {dmg} damage!', flush=True)
         if self.stats[Stat.HP]<=0:
