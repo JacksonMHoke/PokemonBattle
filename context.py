79c331b14c3a99425b2b1eaf591d58bdc600df2f
JacksonMHoke <jacksonmhoke@gmail.com>
2025-04-07 07:24:00 -0700

    Added documentation for context object

--- context.py
+++ context.py
@@ -1,4 +1,17 @@
 class Context:
+    """
+    Holds all battle context
+
+    Attributes:
+        turn (int): turn number
+        attacker (Pokemon): attacking pokemon
+        attackerLoc (BattleLocation): attacker location
+        defenders (list): list of defending pokemon
+        defenderLocs (list): list of defending pokemon locations
+        move (Move): move being used right now
+        effects (list): list of effects
+        teams (list): list of teams
+    """
     def __init__(self, teams):
         self.turn=0
         self.attacker=None
@@ -10,19 +23,23 @@ class Context:
         self.teams=teams
     
     def setAttacker(self, attackerLoc):
+        """Updates context for new attacker"""
         self.attacker=attackerLoc.pokemonAtSelection
         self.attackerLoc=attackerLoc
     
     def setDefenders(self, defenderLocs):
+        """Updates context for new defenders"""
         self.defenders=[loc.pokemon for loc in defenderLocs]
         self.defenderLocs=defenderLocs
     
     def addDefender(self, defender):
+        """Updates context for an additional defender"""
         if self.defenders is None:
             self.defenders=[]
         self.defenders.append(defender)
 
     def prepareMove(self, attackerLoc, defenderLocs, move):
+        """Prepares context for enacting move"""
         self.move=move
         self.setAttacker(attackerLoc)
         self.setDefenders(defenderLocs)
