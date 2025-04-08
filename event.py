23f22c21e93bb08af1e01fc4c01849e367edb307
JacksonMHoke <jacksonmhoke@gmail.com>
2025-04-08 14:07:56 -0700

    Documents items. Finishes #15

--- event.py
+++ event.py
@@ -1,7 +1,8 @@
 from abc import ABC, abstractmethod
 from globals import *
 
-def triggerAllEvents(context, trigger):
+def triggerAllEvents(context, trigger):                                 # TODO: Make triggerAllEvents not only trigger all items and abiltiies
+    """Triggers all events"""
     context.trigger=trigger
     for team in context.teams:
         for trainer in team.trainers:
@@ -13,24 +14,45 @@ def triggerAllEvents(context, trigger):
                     pokemon.ability.trigger(context)
 
 class Event(ABC):
+    """
+    Event that can be triggered from specific trigger
+
+    Attributes:
+        triggers (list): list of triggers that trigger event
+    """
     def __init__(self, triggers):
         self.triggers=triggers
 
     def trigger(self, context):
+        """Attempt to trigger event based on current context trigger"""
         pass
 
 class Item(Event):
+    """
+    Item that triggers in battle
+
+    Attributes:
+        name (str): name of item
+        timesUsed (int): number of times item has been triggered
+    """
     def __init__(self, name, triggers):
         super().__init__(triggers)
         self.name=name
         self.timesUsed=0
 
 class Ability(Event):
+    """
+    Ability that triggers in battle
+
+    Attributes:
+        name (str): name of ability
+    """
     def __init__(self, name, triggers):
         super().__init__(triggers)
         self.name=name
 
 class Sword(Item):
+    """Item that buffs attack by a flat amount"""
     def __init__(self):
         triggers=[Trigger.START]
         super().__init__(name=self.__class__.__name__, triggers=triggers)
