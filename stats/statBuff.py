5d523430311ca3d408224cdd6b84b1779bf24f9c
JacksonMHoke <jacksonmhoke@gmail.com>
2025-07-06 17:32:36 -0700

    Finished base implementation of stats system

--- stats/statBuff.py
+++ stats/statBuff.py
@@ -8,6 +8,9 @@ class StatBuff:
 
     def isActive(self, currentTurn):
         return True
+    
+    def isExpired(self, currentTurn):
+        return False
 
 class TimedStatBuff(StatBuff):
     def __init__(self, startTurn, duration, name=None, flat=0, mult=0):
@@ -17,3 +20,6 @@ class TimedStatBuff(StatBuff):
 
     def isActive(self, currentTurn):
         return self.startTurn<=currentTurn<self.startTurn+self.duration
+    
+    def isExpired(self, currentTurn):
+        return self.startTurn+self.duration<=currentTurn
--- stats/stats.py
+++ stats/stats.py
@@ -1,5 +1,6 @@
 from globals import *
 from collections import defaultdict
+from collections.abc import Iterable
 
 class Stats:
     """Stats object that stores a pokemon's stats.
@@ -19,7 +20,7 @@ class Stats:
         effectiveSpd (int): Pokemon's effective special defense
         effectiveSpe (int): Pokemon's effective speed
         currentHp (int): Pokemon's current hp
-        buffs (Dict[str : StatBuff]): Dictionary of buffs by stat
+        buffs (Dict[str : list[StatBuff]]): Dictionary of buffs by stat
     """
     def __init__(self, HP=0, ATT=0, SPA=0, DEF=0, SPD=0, SPE=0):
         self.baseMaxHp=HP
@@ -28,7 +29,7 @@ class Stats:
         self.baseDef=DEF
         self.baseSpd=SPD
         self.baseSpe=SPE
-        self.currentHp=HP
+        self._currentHp=HP
         self.statNames=("Hp", "Att", "Spa", "Def", "Spd", "Spe")
         self.buffs=defaultdict(list)
 
@@ -39,23 +40,53 @@ class Stats:
 
     def __getattr__(self, name):
         if name.startswith('effective'):
-            stat=name[9:]
+            stat=name[len('effective'):]
             baseStat=self.__dict__[f'base{stat}']
             mult=1.0
             flat=0
-            for buff in self.buffs[stat]:
+            for buff in self.getActiveBuffs(stat):
                 flat+=buff.flat
                 mult+=buff.mult
             return (baseStat+flat)*mult
         if name=='currentHp':
-            return min(self.__dict__[name], self.effectiveMaxHp)
-        raise AttributeError(f'{Stats.__name__} does not have attribute {name}')
+            self._currentHp=min(self._currentHp, self.effectiveMaxHp)
+            return self._currentHp
+        raise AttributeError(f'{self.__class__.__name__} does not have attribute {name}')
     
     def addBuff(self, buff):
+        if buff.stat not in self.statNames:
+            raise ValueError(f"Unknown stat '{buff.stat}' in buff.")
         self.buffs[buff.stat].append(buff)
 
-    def removeExpiredBuffs(self):
-        pass
+    def removeExpiredBuffs(self, stat=None):
+        if stat is None:
+            stat=self.statNames
+        if not isinstance(stat, Iterable):
+            stat=[stat]
+        for s in stat:
+            self.buffs[s]=[buff for buff in self.buffs[s] if not buff.isExpired(self.battleContext.turn)]
 
     def getActiveBuffs(self, stat):
-        pass
+        self.removeExpiredBuffs(stat=stat)
+        return [buff for buff in self.buffs[stat] if buff.isActive(self.battleContext.turn)]
+    
+    def removeBuffs(self, shouldRemoveFn, stat=None):
+        if stat is None:
+            stat=self.statNames
+        if not isinstance(stat, Iterable):
+            stat=[stat]
+        for s in stat:
+            self.buffs[s]=[buff for buff in self.buffs[s] if not shouldRemoveFn(buff)]
+
+    @property
+    def battleContext(self):
+        if not hasattr(self, '_battleContext') or self._battleContext is None:
+            raise AttributeError(f'{self.__class__.__name__} is missing battleContext.')
+        return self._battleContext
+    
+    @battleContext.setter
+    def battleContext(self, val):
+        self._battleContext=val
+
+    def setBattleContext(self, battleContext):
+        self.battleContext=battleContext
